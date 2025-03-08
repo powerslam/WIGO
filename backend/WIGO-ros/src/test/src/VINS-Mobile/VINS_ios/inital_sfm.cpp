1a51af6589ead2d96ca1197b1d49a0c35e26e510
powerslam <timsort@kookmin.ac.kr>
2025-03-08 16:34:35 +0900

    feat : linear solve error 위치 발견

--- backend/WIGO-ros/src/test/src/VINS-Mobile/VINS_ios/inital_sfm.cpp
+++ backend/WIGO-ros/src/test/src/VINS-Mobile/VINS_ios/inital_sfm.cpp
@@ -1,4 +1,5 @@
 #include "inital_sfm.hpp"
+#include <ros/ros.h>
 
 GlobalSFM::GlobalSFM(){}
 
@@ -278,14 +279,18 @@ bool GlobalSFM::construct(int frame_num, Quaterniond* q, Vector3d* T, int l,
         
     }
     ceres::Solver::Options options;
-    options.linear_solver_type = ceres::DENSE_SCHUR;
+    options.linear_solver_type = ceres::DENSE_SCHUR; // SPARSE_SCHUR
     //options.minimizer_progress_to_stdout = true;
     options.max_solver_time_in_seconds = 0.3;
     ceres::Solver::Summary summary;
+    ROS_INFO("@@@@ solve 4 start @@@@");
     ceres::Solve(options, &problem, &summary);
-    // std::cout << summary.BriefReport() << "\n";
-    if (summary.termination_type == ceres::CONVERGENCE || summary.final_cost < 3e-03)
+    ROS_INFO("@@@@ solve 4 end @@@@");
+
+    std::cout << summary.BriefReport() << "\n";
+    if (summary.termination_type == ceres::CONVERGENCE || summary.final_cost < 3e-03){
         cout << "vision only BA converge" << endl;
+    }
     else
     {
         cout << "vision only BA not converge " << endl;
