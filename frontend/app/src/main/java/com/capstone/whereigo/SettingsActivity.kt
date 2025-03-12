8b91e1c43153cb26a5c35a6636dd01f025795bf9
sungjun <kkk121026@naver.com>
2025-03-12 17:52:30 +0900

    feat: download_map, setting connect

--- frontend/app/src/main/java/com/capstone/whereigo/SettingsActivity.kt
+++ frontend/app/src/main/java/com/capstone/whereigo/SettingsActivity.kt
@@ -1,13 +1,21 @@
-package com.capstone.whereigo
+package com.example.test_map
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
         if (savedInstanceState == null) {
             supportFragmentManager
@@ -16,11 +24,32 @@ class SettingsActivity : AppCompatActivity() {
                 .commit()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // ViewCompat.setOnApplyWindowInsetsListener 를 onCreate 안으로 이동
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onMapItemClick(preference: Preference) {
+        val intent = Intent(this, DownloadActivity::class.java)
+        startActivity(intent)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            val mapItemPreference: Preference? = findPreference("map_download")
+
+            mapItemPreference?.setOnPreferenceClickListener {
+                (activity as SettingsActivity).onMapItemClick(it)
+                true
+            }
         }
+
+
     }
 }
