f21001ac8328efb0d7404d9e7c8274b9076487dd
Josungjun <kkk121026@naver.com>
2025-04-11 22:24:57 +0900

    del: computeBRIEFPoint 주석 처리
    현재 BRIEF 패턴 파일 경로를 빈 문자열로 전달하여 에러가 발생함.

--- backend/pose_graph/src/keyframe.cpp
+++ backend/pose_graph/src/keyframe.cpp
@@ -29,7 +29,7 @@ KeyFrame::KeyFrame(double _time_stamp, int _index, Vector3d &_vio_T_w_i, Matrix3
 	loop_info << 0, 0, 0, 0, 0, 0, 0, 0;
 	sequence = _sequence;
 	// computeWindowBRIEFPoint();
-	computeBRIEFPoint();
+	// computeBRIEFPoint();
 }
 
 // load previous keyframe
