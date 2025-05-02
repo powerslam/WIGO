f0d15c2e30eec52d9e2f3ec7d1eac3c6df9f5728
SeoooooNyeong <bm9024@naver.com>
2025-05-02 13:20:51 +0900

    refactor: SearchResultAdapter java로 변경

---
+++ frontend/app/src/main/java/com/capstone/whereigo/SearchResultAdapter.java
@@ -0,0 +1,49 @@
+package com.capstone.whereigo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.capstone.whereigo.R;
+
+import java.util.List;
+
+public class SearchResultAdapter extends RecyclerView.Adapter<SearchResultAdapter.ViewHolder> {
+
+    private final List<String> items;
+
+    public SearchResultAdapter(List<String> items) {
+        this.items = items;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+
+        public ViewHolder(View view) {
+            super(view);
+            textView = view.findViewById(R.id.itemText);
+        }
+    }
+
+    @NonNull
+    @Override
+    public SearchResultAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_search_result, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SearchResultAdapter.ViewHolder holder, int position) {
+        holder.textView.setText(items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+}
