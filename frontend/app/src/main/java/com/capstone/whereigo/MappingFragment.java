cb87af17faf0ed931fb8b7d6f0d85c262b36f001
powerslam <timsort@kookmin.ac.kr>
2025-05-10 23:48:04 +0900

    feat: Mapping을 위한 기본 화면 렌더링 로직

---
+++ frontend/app/src/main/java/com/capstone/whereigo/MappingFragment.java
@@ -0,0 +1,35 @@
+package com.capstone.whereigo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.capstone.whereigo.databinding.FragmentMappingBinding;
+
+public class MappingFragment extends Fragment {
+    private FragmentMappingBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentMappingBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
