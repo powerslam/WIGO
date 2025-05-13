91b4bc6e2473875ea23d8a5e5d5dcf758df9493c
powerslam <timsort@kookmin.ac.kr>
2025-05-14 00:38:37 +0900

    feat: 건물 정보 관리를 위한 데이터 추가

--- frontend/app/src/main/java/com/capstone/whereigo/PoseStampViewModel.java
+++ frontend/app/src/main/java/com/capstone/whereigo/PoseStampViewModel.java
@@ -14,6 +14,28 @@ public class PoseStampViewModel extends ViewModel {
     private final MutableLiveData<List<PoseStamp>> poseStampList = new MutableLiveData<>();
     private final MutableLiveData<List<String>> poseStampLabelList = new MutableLiveData<>();
 
+    private String buildingName = "";
+    private String floorName = "";
+
+    private int floorMinIdx = 0;
+    private final String[] floorMinItem = {
+            "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10"
+    };
+
+    private int floorMaxIdx = 0;
+    private final String[] floorMaxItem = {
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+            "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+            "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
+            "41", "42", "43", "44", "45", "46", "47", "48", "49", "50",
+            "51", "52", "53", "54", "55", "56", "57", "58", "59", "60",
+            "61", "62", "63", "64", "65", "66", "67", "68", "69", "70",
+            "71", "72", "73", "74", "75", "76", "77", "78", "79", "80",
+            "81", "82", "83", "84", "85", "86", "87", "88", "89", "90",
+            "91", "92", "93", "94", "95", "96", "97", "98", "99", "100"
+    };
+
     public PoseStampViewModel() {
         List<PoseStamp> initPoseStampList = new ArrayList<>();
         poseStampList.setValue(initPoseStampList);
@@ -49,6 +71,19 @@ public class PoseStampViewModel extends ViewModel {
         }
         return null;
     }
+
+    public String getBuildingName(){ return this.buildingName; }
+
+    public int getFloorMinIdx(){ return this.floorMinIdx; }
+    public String[] getFloorMinItem() { return this.floorMinItem; }
+    public void updateFloorMinIdx(int index) { this.floorMinIdx = index; }
+
+    public int getFloorMaxIdx(){ return this.floorMaxIdx; }
+    public String[] getFloorMaxItem() { return this.floorMaxItem; }
+    public void updateFloorMaxIdx(int index) { this.floorMaxIdx = index; }
+
+    public String getFloorName(){ return this.floorName; }
+    public void updateFloorName(String floorName){ this.floorName = floorName; }
 
     public void updateLabel(int index, String newLabel) {
         List<String> list = poseStampLabelList.getValue();
